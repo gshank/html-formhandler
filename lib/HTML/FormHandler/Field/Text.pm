9b09f8d3ade19bb3216aea009f54c2e584010461
Zbigniew Lukasiak <zby@zby.aster.net.pl>
2009-07-01 17:07:44 +0200

    minlength instead of min_length for consistency with maxlength

--- lib/HTML/FormHandler/Field/Text.pm
+++ lib/HTML/FormHandler/Field/Text.pm
@@ -6,7 +6,17 @@ our $VERSION = '0.01';
 
 has 'size' => ( isa => 'Int|Undef', is => 'rw', default => '0' );
 has 'maxlength' => ( isa => 'Int|Undef', is => 'rw' );
-has 'min_length' => ( isa => 'Int|Undef', is => 'rw', default => '0' );
+has 'minlength' => ( isa => 'Int|Undef', is => 'rw', default => '0' );
+has 'min_length' => ( isa => 'Int|Undef', is => 'rw', default => '0', reader => '_min_length_r', writer => '_min_length_w' ); # for backcompat
+sub min_length {
+    my ( $self, $value ) = @_;
+    warn "Please use the 'minlength' attribute - 'min_length' is deprecated";
+    if( $value ){
+        $self->_min_length_w($value);
+    }
+    return $self->_min_length_r;
+}
+
 has '+widget' => ( default => 'text' );
 
 sub validate {
@@ -21,7 +31,7 @@ sub validate {
     }
 
     # Check for min length
-    if ( my $size = $field->min_length  ) {
+    if ( my $size = $field->minlength || $field->_min_length_r ) {
         return $field->add_error( 
            'Input must be at least [quant,_1,character]. You submitted [_2]', 
            $size, length $value )
