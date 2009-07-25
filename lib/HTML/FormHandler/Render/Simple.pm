8172e526166857d8f8be1dc211f1f04ca1982dc1
Gerda Shank <gerda.shank@gmail.com>
2009-07-25 12:57:06 -0400

    Remove name from form (fails XHTML validation)

--- lib/HTML/FormHandler/Render/Simple.pm
+++ lib/HTML/FormHandler/Render/Simple.pm
@@ -152,7 +152,6 @@ sub render_start
    my $output = '<form ';
    $output .= 'action="' . $self->action . '" ' if $self->action;
    $output .= 'id="' . $self->name . '" ' if $self->name;
-   $output .= 'name="' . $self->name . '" ' if $self->name;
    $output .= 'method="' . $self->http_method . '"' if $self->http_method;
    $output .= '>' . "\n";
    $output .= '<fieldset class="main_fieldset">' if $self->auto_fieldset;
