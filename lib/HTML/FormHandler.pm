7aeb1db294bf46e3466b2648a92788f9410bf4f9
t0m <bobtfish@bobtfish.net>
2009-08-07 13:22:10 +0100

    Make example a little easier to cargo cult

--- lib/HTML/FormHandler.pm
+++ lib/HTML/FormHandler.pm
@@ -28,12 +28,16 @@ An example of a form class:
 
     use HTML::FormHandler::Moose;
     extends 'HTML::FormHandler';
+    with 'HTML::FormHandler::Renderer::Simple';
 
     has '+item_class' => ( default => 'User' );
 
     has_field 'name' => ( type => 'Text' );
     has_field 'age' => ( type => 'PosInteger', apply => [ 'MinimumAge' ] );
     has_field 'birthdate' => ( type => 'DateTime' );
+    has_field 'birthdate.month' => ( type => 'Month' ); # Explicitly split
+    has_field 'birthdate.day' => ( type => 'MonthDay' ); # fields for renderer
+    has_field 'birthdate.year' => ( type => 'Year' );
     has_field 'hobbies' => ( type => 'Multiple' );
     has_field 'address' => ( type => 'Text' );
     has_field 'city' => ( type => 'Text' );
