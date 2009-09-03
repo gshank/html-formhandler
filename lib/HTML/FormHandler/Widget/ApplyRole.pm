bd3a5c9c3439b7628a5fa015800fde6d3c7888a3
Gerda Shank <gerda.shank@gmail.com>
2009-09-03 11:18:46 -0400

    use Class::Load in ApplyRole

--- lib/HTML/FormHandler/Widget/ApplyRole.pm
+++ lib/HTML/FormHandler/Widget/ApplyRole.pm
@@ -2,6 +2,7 @@ package HTML::FormHandler::Widget::ApplyRole;
 
 use Moose::Role;
 use File::Spec;
+use Class::Load ('try_load_class' );
 
 our $ERROR;
 
@@ -39,37 +40,6 @@ sub widget_class {
     $widget =~ s/^(\w{1})/\u$1/g;
     $widget =~ s/_(\w{1})/\u$1/g;
     return $widget;
-}
-
-# stolen from Load::Class
-# which has been broken on 5.10 for months
-sub try_load_class {
-    my $class = shift;
-    undef $ERROR;
-    return 1 if is_class_loaded($class);
-    # see rt . perl . org    #19213
-    my @parts = split '::', $class;
-    my $file =
-        $^O eq 'MSWin32' ?
-        join '/', @parts :
-        File::Spec->catfile(@parts);
-    $file .= '.pm';
-
-    return 1 if eval {
-        local $SIG{__DIE__} = 'DEFAULT';
-        require $file;
-        1;
-    };
-
-    $ERROR = $@;
-    return 0;
-}
-
-sub is_class_loaded {
-    my $class = shift;
-    # is the module's file in %INC?
-    my $file = ( join '/', split '::', $class ) . '.pm';
-    return 1 if $INC{$file};
 }
 
 1;
