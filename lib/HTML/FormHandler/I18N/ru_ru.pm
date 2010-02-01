38fbebb83fbdc26ea3b3aecce440a9af7ba54e48
Oleg Kostyuk <cub.uanic@gmail.com>
2010-02-02 05:56:50 +0800

    Added Russian and Ukrainian message files

---
+++ lib/HTML/FormHandler/I18N/ru_ru.pm
@@ -0,0 +1,89 @@
+package HTML::FormHandler::I18N::ru_ru;
+use strict;
+use warnings;
+use utf8;
+use base 'HTML::FormHandler::I18N';
+
+# Auto define lexicon
+our %Lexicon = (
+    '_AUTO' => 1,
+
+    # H::F::Field::Date
+    'Date is too early' => 'Слишком ранняя дата',
+    'Date is too late'  => 'Слишком поздняя дата',
+
+    # H::F::Field::DateTime
+    'Not a valid DateTime' => 'Неверная дата/время',
+
+    # H::F::Field::Email
+    'Email should be of the format [_1]' => 'Адрес электронной почты должен быть в формате [_1]',
+
+    # H::F::Field::Integer
+    'Value must be an integer' => 'Значение должно быть целым числом',
+
+    # H::F::Field::Money
+    'Value cannot be converted to money' => 'Значение не может быть воспринято как денежное',
+    'Value must be a real number'        => 'Значение должно быть вещественным числом',
+
+    # H::F::Field::Password
+    'Please enter a password in this field' => 'Пожалуйста, введите пароль',
+
+    # H::F::Field::PasswordConf
+    'Please enter a password confirmation' => 'Пожалуйста, введите подтверждение пароля',
+
+    # H::F::Field::PosInteger
+    'Value must be a positive integer' => 'Значение должно быть положительным целым числом',
+
+    # H::F::Field::Select
+    'This field does not take multiple values' => 'Это поле не принимает несколько значений',
+
+    # H::F::Field::Text
+    'Please limit to [quant,_1,character]. You submitted [_2]'        => 'Символов должно быть не более [_1]. Вы ввели: [_2]',
+    'Input must be at least [quant,_1,character]. You submitted [_2]' => 'Символов должно быть не менее [_1]. Вы ввели: [_2]',
+
+    # H::F::Field::Upload
+    'File uploaded is empty'           => 'Переданный файл пуст',
+    'File is too small (< [_1] bytes)' => 'Файл слишком мал (менее [_1] байт)',
+    'File is too big (> [_1] bytes)'   => 'Файл слишком велик (более [_1] байт)',
+
+    # H::F::Field
+    'field is invalid' => 'Поле неверно',
+
+    # H::F::Model
+    'Value must be unique in the database' => 'Значение должно быть уникальным для базы данных',
+
+    # H::F::Types
+    'Must be a positive number'                                 => 'Должно быть положительным числом',
+    'Must be a positive integer'                                => 'Должно быть положительным целым числом',
+    'Must be a negative number'                                 => 'Должно быть отрицательным числом',
+    'Must be a negative integer'                                => 'Должно быть отрицательным целым числом',
+    'Must be a single digit'                                    => 'Должно быть одной цифрой',
+    'Must be a single line of no more than 255 chars'           => 'Должно быть одной строкой, не более 255 символов',
+    'Must be a non-empty single line of no more than 255 chars' => 'Должно быть не пустой строкой, не более 255 символов',
+    'Must be between 4 and 255 chars'                           => 'Должно быть от 4 до 255 символов',
+    'Not a valid state'                                         => 'Не верное состояние',
+    'Email is not valid'                                        => 'Адрес электронной почты не корректен',
+    'Zip is not valid'                                          => 'Почтовый индекс не корректен',
+    'Not a valid IP address'                                    => 'IP адрес не корректен',
+    'Cannot contain spaces'                                     => 'Не может содердать пробелы',
+    'Must be made up of letters, digits, and underscores'       => 'Должно состоять из букв, цифр и подчёркиваний',
+    'Must not be all digits'                                    => 'Должно состоять не только из цифр',
+    'Field contains non-printable characters'                   => 'Поле содержит непечатаемые символы',
+    'Field must contain a single word'                          => 'Поле должно содержать одно слово',
+
+    # H::F::Validate::Actions
+    'Wrong value' => 'Неверное значение',
+    ### not translatable: '"$value" does not match' => '',
+    ### not translatable: '"$value" not allowed' => '',
+
+    # H::F::Validate
+    'value must be between [_1] and [_2]'         => 'значение должно быть между [_1] и [_2]',
+    'value must be greater than or equal to [_1]' => 'значение должно быть больше или равно [_1]',
+    'value must be less than or equal to [_1]'    => 'значение должно быть меньше или равно [_1]',
+
+    # Other
+    'Your datetime does not match your pattern.' => 'Введённые дата/время не совпадают с вашим шаблоном.',
+);
+
+1;
+
---
+++ lib/HTML/FormHandler/I18N/ua_ua.pm
@@ -0,0 +1,89 @@
+package HTML::FormHandler::I18N::ua_ua;
+use strict;
+use warnings;
+use utf8;
+use base 'HTML::FormHandler::I18N';
+
+# Auto define lexicon
+our %Lexicon = (
+    '_AUTO' => 1,
+
+    # H::F::Field::Date
+    'Date is too early' => 'Дата занадто рання',
+    'Date is too late'  => 'Дата занадто піздня',
+
+    # H::F::Field::DateTime
+    'Not a valid DateTime' => 'Невірна дата/час',
+
+    # H::F::Field::Email
+    'Email should be of the format [_1]' => 'Адреса электроної поши має бути у форматі [_1]',
+
+    # H::F::Field::Integer
+    'Value must be an integer' => 'Значення має бути цілим числом',
+
+    # H::F::Field::Money
+    'Value cannot be converted to money' => 'Значення не може бути сприйнято як грошове',
+    'Value must be a real number'        => 'Значення має бути речовим числом',
+
+    # H::F::Field::Password
+    'Please enter a password in this field' => 'Будь ласка, введіть пароль',
+
+    # H::F::Field::PasswordConf
+    'Please enter a password confirmation' => 'Будь ласка, введіть підтвердження паролю',
+
+    # H::F::Field::PosInteger
+    'Value must be a positive integer' => 'Значення має бути позитивним цілим числом',
+
+    # H::F::Field::Select
+    'This field does not take multiple values' => 'Це поле не приймає кілька значень',
+
+    # H::F::Field::Text
+    'Please limit to [quant,_1,character]. You submitted [_2]'        => 'Символів має бути не більше [_1]. Ви ввели: [_2]',
+    'Input must be at least [quant,_1,character]. You submitted [_2]' => 'Символів має бути не менше [_1]. Ви ввели: [_2]',
+
+    # H:: F:: Field:: Upload
+    'File uploaded is empty'          => 'Переданий файл порожній',
+    'File is too small (<[_1] bytes)' => 'Файл занадто малий (менше [_1] байт)',
+    'File is too big (> [_1] bytes)'  => 'Файл занадто великий (більше [_1] байт)',
+
+    # H:: F:: Field
+    'field is invalid' => 'Поле невірне',
+
+    # H:: F:: Model
+    'Value must be unique in the database' => 'Значення має бути унікальним для бази даних',
+
+    # H:: F:: Types
+    'Must be a positive number'                                 => 'Має бути позитивним числом',
+    'Must be a positive integer'                                => 'Має бути позитивним цілим числом',
+    'Must be a negative number'                                 => 'Має бути негативним числом',
+    'Must be a negative integer'                                => 'Має бути негативним цілим числом',
+    'Must be a single digit'                                    => 'Має бути однією цифрою',
+    'Must be a single line of no more than 255 chars'           => 'Має бути одним рядком, не більше 255 символів',
+    'Must be a non-empty single line of no more than 255 chars' => 'Має бути не пустим рядком, не більше 255 символів',
+    'Must be between 4 and 255 chars'                           => 'Має бути від 4 до 255 символів',
+    'Not a valid state'                                         => 'Не вірний стан',
+    'Email is not valid'                                        => 'Адреса електронної пошти не коректна',
+    'Zip is not valid'                                          => 'Поштовий індекс не коректний',
+    'Not a valid IP address'                                    => 'IP адреса не коректна',
+    'Cannot contain spaces'                                     => 'Не може мати пробіли',
+    'Must be made up of letters, digits, and underscores'       => 'Має складатися з букв, цифр та підкреслень',
+    'Must not be all digits'                                    => 'Має бути не тільки з цифр',
+    'Field contains non-printable characters'                   => 'Поле містить недруковані символи',
+    'Field must contain a single word'                          => 'Поле має містити одне слово',
+
+    # H:: F:: Validate:: Actions
+    'Wrong value' => 'Неправильне значення',
+    # # # Not translatable: ' "$ value" does not match' =>'',
+    # # # Not translatable: ' "$ value" not allowed' =>'',
+
+    # H:: F:: Validate
+    'value must be between [_1] and [_2]'         => 'значення повинне бути між [_1] та [_2]',
+    'value must be greater than or equal to [_1]' => 'значення повинне бути більше або дорівнювати [_1]',
+    'value must be less than or equal to [_1]'    => 'значення повинно бути менше або дорівнювати [_1]',
+
+    # Other
+    'Your datetime does not match your pattern.' => 'Введені дата/час не співпадають з вашим шаблоном.',
+);
+
+1;
+
