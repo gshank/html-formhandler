ab96a0ddef9b2445ae4f5fe58c2df8aae57984af
Gerda Shank <gerda.shank@gmail.com>
2012-04-16 15:01:25 -0400

    minor rendering doc updates

--- lib/HTML/FormHandler/Widget/Wrapper/None.pm
+++ lib/HTML/FormHandler/Widget/Wrapper/None.pm
@@ -1,6 +1,13 @@
 package HTML::FormHandler::Widget::Wrapper::None;
 # ABSTRACT: wrapper that doesn't wrap
 
+=head1 DESCRIPTION
+
+This wrapper does nothing except return the 'bare' rendered form element,
+as returned by the 'widget'. It does not add errors or anything else.
+
+=cut
+
 use Moose::Role;
 
 sub wrap_field { "\n" . $_[2] }
