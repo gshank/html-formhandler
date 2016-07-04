214f6ff9961fd17ddd0d4e3d5c67d9968fa5d2ad
Michael <michael@michaeldesk.foxtons.co.uk>
2016-07-04 12:58:23 +0100

    Add comma digit grouping and custom currency symbol

--- lib/HTML/FormHandler/Field/Money.pm
+++ lib/HTML/FormHandler/Field/Money.pm
@@ -6,6 +6,16 @@ extends 'HTML::FormHandler::Field::Text';
 our $VERSION = '0.01';
 
 has '+html5_type_attr' => ( default => 'number' );
+has 'currency_symbol' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => '$',
+);
+has 'allow_commas' => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
+);
 
 our $class_messages = {
     'money_convert' => 'Value cannot be converted to money',
@@ -21,30 +31,43 @@ sub get_class_messages  {
 }
 
 apply(
-    [
-        {
+    [   {   # remove any leading currency symbol
             transform => sub {
-                my $value = shift;
-                $value =~ s/^\$//;
+                my ( $value, $field ) = @_;
+                my $c = $field->currency_symbol;
+                $value =~ s/^\Q$c\E// if $c;
                 return $value;
-                }
+            },
         },
-        {
+        {   # check number looks real, optionally allow comma digit groupings
+            check => sub {
+                my ( $value, $field ) = @_;
+                return $field->allow_commas
+                     ? $value =~ /^[-+]?(?:\d+|\d{1,3}(,\d{3})*)(?:\.\d+)?$/
+                     : $value =~ /^[-+]?\d+(?:\.\d+)?$/;
+            },
+            message => sub {
+                my ( $value, $field ) = @_;
+                return [ $field->get_message('money_real'), $value ];
+            },
+        },
+        {   # remove commas
+            transform => sub {
+                my ($value, $field) = @_;
+                $value =~ tr/,//d if $field->allow_commas;
+                return $value;
+            },
+        },
+        {   # convert to standard number, formatted to 2 decimal palaces
             transform => sub { sprintf '%.2f', $_[0] },
-            message => sub {
+            message   => sub {
                 my ( $value, $field ) = @_;
-                return [$field->get_message('money_convert'), $value];
+                return [ $field->get_message('money_convert'), $value ];
             },
         },
-        {
-            check => sub { $_[0] =~ /^-?\d+\.?\d*$/ },
-            message => sub {
-                my ( $value, $field ) = @_;
-                return [$field->get_message('money_real'), $value];
-            },
-        }
     ]
 );
+
 
 =head1 DESCRIPTION
 
