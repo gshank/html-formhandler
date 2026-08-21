bce6c60236e26a2c2d563cd7cdd30f0610dec237
Robert Rothenberg <perl@rhizomnic.com>
2026-08-21 09:55:21 +0100

    Revert "HTML::FormHandler: stop routing foreign text into the Locale::Maketext format"
    This reverts commit c7c69bb58883c0ab9dd7e5dbfa59c0eb11b36750.

--- lib/HTML/FormHandler/Field/Date.pm
+++ lib/HTML/FormHandler/Field/Date.pm
@@ -130,11 +130,7 @@ sub validate {
 
     my $dt = eval { $strp->parse_datetime( $self->value ) };
     unless ($dt) {
-        # The parser's message is not ours to hand to the localizer as a
-        # bracket-notation FORMAT. DateTime::Format::Strptime 1.80 does not
-        # quote the rejected input into errmsg, but that is the parser's text
-        # to change, and the `|| $@` fallback is a second channel.
-        $self->add_error( $self->_escape_bracket_notation( $strp->errmsg || $@ ) );
+        $self->add_error( $strp->errmsg || $@ );
         return;
     }
     $self->_set_value($dt);
