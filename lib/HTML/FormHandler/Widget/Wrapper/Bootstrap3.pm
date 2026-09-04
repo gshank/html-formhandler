31ba304c214bb80ac1e756fbd2b4749ae2ab26a1
Alexander Hartmaier <alex@hartmaier.priv.at>
2026-09-04 09:07:30 +0200

    fix CVE-2026-85485
    Three locations of unencoded error messages have been overlooked in the fix for CVE-2026-19872.
    These where in the Table form layout and Bootstrap (2 and 3) theme wrappers.

--- lib/HTML/FormHandler/Widget/Wrapper/Bootstrap3.pm
+++ lib/HTML/FormHandler/Widget/Wrapper/Bootstrap3.pm
@@ -5,6 +5,7 @@ use Moose::Role;
 use namespace::autoclean;
 use HTML::FormHandler::Render::Util ('process_attrs');
 use List::Util 1.33 ('any');
+use HTML::Entities qw( encode_entities );
 
 with 'HTML::FormHandler::Widget::Wrapper::Base';
 
@@ -88,7 +89,7 @@ sub wrap_field {
     # various 'help-inline' bits: errors, warnings
     unless( $self->get_tag('no_errors') ) {
         $output .= qq{\n<span class="help-block">$_</span>}
-            for $result->all_errors;
+            for map { encode_entities($_) } $result->all_errors;
         $output .= qq{\n<span class="help-block">$_</span>} for $result->all_warnings;
     }
     # extra after element stuff
